2e1a71a1615fe2adc2c2cd0726f45d8210947cf2
Aleksey Batnin <al-bat-ros@mail.ru>
2019-11-29 22:27:54 +0500

    Promis

--- script/script.js
+++ script/script.js
@@ -414,22 +414,25 @@ window.addEventListener('DOMContentLoaded', function () {
 
             
 
-              const postData = (body, outputData, errorData) => {
-                const request = new XMLHttpRequest();
-                request.addEventListener('readystatechange', () => {               
-                    if(request.readyState !== 4) {
-                        return;
-                    }
-                    if (request.status === 200){
-                        outputData(request.status);
-                    } else {
-                        errorData(); 
-                    }
-                 });
-
-                request.open('POST', './server.php');
-                request.setRequestHeader('Content-Type', 'application/json');
-                request.send(JSON.stringify(body));
+              const postData = (body, errorData) => {
+                  return new Promise((resolve,reject) => {
+                    const request = new XMLHttpRequest();
+                    request.addEventListener('readystatechange', () => {               
+                        if(request.readyState !== 4) {
+                            return;
+                        }
+                        if (request.status === 200){
+                            resolve(request.status);
+                        } else {
+                            reject(request.statusText); 
+                        }
+                     });
+    
+                    request.open('POST', './server.php');
+                    request.setRequestHeader('Content-Type', 'application/json');
+                    request.send(JSON.stringify(body));
+                  });
+ 
               };
 
     };
